ff2588c1d729e1a9ef42056bc2b34661a25d5107
TDD-Guru-Arjun <ajuabi13@gmail.com>
2024-02-02 16:25:13 +0530

    Refactor and simplify those unit tests for intro.js

--- src/intro.test.js
+++ src/intro.test.js
@@ -3,34 +3,28 @@ import { fizzBuzz, max } from "./intro"
 
 describe('max', () => {
     it('should return the b if b > a', () => {
-        const actualResult = max(1, 2)
-        expect(actualResult).toBe(2)
+        expect(max(1, 2)).toBe(2)
     })
 
     it('should return the a if a > b', () => {
-        const actualResult = max(10, 2)
-        expect(actualResult).toBe(10)
+        expect(max(10, 2)).toBe(10)
     })
 
     it('should return the a or b if both are same', () => {
-        const actualResult = max(3, 3)
-        expect(actualResult).toBe(3)
+        expect(max(3, 3)).toBe(3)
     })
 })
 
 describe('fizzBuzz', () => {
     it('should return FizzBuzz if n is divisible by both 3 and 5', () => {
-        const actualResult = fizzBuzz(15)
-        expect(actualResult).toBe("FizzBuzz")
+        expect(fizzBuzz(15)).toBe("FizzBuzz")
     })
 
     it('should return Fizz if n is divisible by 3', () => {
-        const actualResult = fizzBuzz(6)
-        expect(actualResult).toBe("Fizz")
+        expect(fizzBuzz(6)).toBe("Fizz")
     })
 
     it('should return Buzz if n is divisible by 5', () => {
-        const actualResult = fizzBuzz(10)
-        expect(actualResult).toBe("Buzz")
+        expect(fizzBuzz(10)).toBe("Buzz")
     })
 })
