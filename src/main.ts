7f18b2a3889e59f7f0d6825e0e1780fce73ed677
TDD-Guru-Arjun <ajuabi13@gmail.com>
2024-02-06 00:36:21 +0530

    Eslint is not configured for both vanilla js and ts.

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,16 @@
 export function calculateDiscount(price: number, discountCode: string) {
-  if(typeof price !== "number" || price <= 0) {
+  if (typeof price !== "number" || price <= 0) {
     return "Invalid price";
   }
 
-  if(typeof discountCode !== "string") {
+  if (typeof discountCode !== "string") {
     return "Invalid discount code";
   }
 
   let discount = 0;
-  if(discountCode === "SAVE10") {
+  if (discountCode === "SAVE10") {
     discount = 0.1;
-  } else if(discountCode === "SAVE20") {
+  } else if (discountCode === "SAVE20") {
     discount = 0.2;
   }
 
